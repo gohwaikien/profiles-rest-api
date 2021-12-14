0d8f9127a1693c43618be384b23b88d3debf54b7
Waikien <gohwaikien@gmail.com>
2021-12-14 16:36:21 +0800

    Fixed Allowed Host Issue

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%!kujt8^yox(4rkdoy+z)m)5^c4si46d2toardo4fn@4k^7bsa'
 DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
-    'http://ec2-13-251-38-245.ap-southeast-1.compute.amazonaws.com',
+    'ec2-13-251-38-245.ap-southeast-1.compute.amazonaws.com',
     '127.0.0.1'
 ]
 
